79afac6e8509134ed4200aa23172d8f4fb466e31
yuuki1036 <yuuki1036@gmail.com>
2021-04-01 07:27:24 +0900

    create static template

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log(process.env.NODE_ENV);
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.disable("x-powered-by");
+
+app.use(
+  "/public",
+  express.static(
+    __dirname +
+      "/public/" +
+      (process.env.NODE_ENV === "development" ? "development" : "production")
+  )
+);
+
+app.use("/", require("./routes/index"));
+
+app.listen(3000);
